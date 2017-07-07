8b039b0b4ee3a2d8c3e630fec6d85e4c5faf4c07
Slawomir Jarzabek <s.g.jarzabek@gmail.com>
2017-07-07 21:53:02 +0200

    dodanie komentarzy style.js

--- js/style.js
+++ js/style.js
@@ -1,32 +1,28 @@
 $(document).ready(function () {
 
+    //Przypisanie pozycji sideMenu po załadowaniu strony do zmiennej a następnie przesunięcie menu poza ekran
     var p1 = $("#sideMenu");
     var offset1 = p1.offset();
-    console.log(offset1);
-
     $('#sideMenu').animate({right: "+=150"}, 1);
 
-    var p2 = $("#sideMenu");
-    var offset2 = p2.offset();
-
-
     $('#sideMenu').mouseover(function() {
+        //przypisanie do zmiennej pozycji sideMenu po jego schowaniu a po najechaniu myszką na ukryte menu jego wysunięcie
         var p1 = $("#sideMenu");
         var offset2 = p1.offset();
-        console.log(offset1);
         $("#sideMenu").offset({ top: offset2.top, left: offset1.left});
     })
 
     $('#sideMenu').mouseleave(function() {
+        //ponowne przypianie pozycji dozmiennej i po opuszczeniu kursora ukrycie sideMenu
         var p1 = $("#sideMenu");
         var offset3 = p1.offset();
         $("#sideMenu").offset({ top: offset3.top, left: offset1.left});
         $("#sideMenu").offset({ top: offset3.top, left: -151.916671752929688});
     });
 
+    //dodanie czarnego podświetlenia do aktywnego elementu sideMenu
     $('.nav li').click(function () {
         $('.nav li').removeClass('active');
         $(this).addClass('active');
     })
-
 });
