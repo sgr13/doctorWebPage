9fa3caecadbcfc820e4c8545289b162089b3a4c9
Slawomir Jarzabek <s.g.jarzabek@gmail.com>
2017-07-07 21:44:30 +0200

    usuwanie niepotrzebnych plikow, dziala na chrome

--- js/style.js
+++ js/style.js
@@ -1,42 +1,27 @@
-//function htmlbodyHeightUpdate(){
-//    var height3 = $( window ).height();
-//    var height1 = $('.nav').height()+50;
-//    height2 = $('.main').height();
-//    if(height2 > height3){
-//        $('html').height(Math.max(height1,height3,height2)+10);
-//        $('body').height(Math.max(height1,height3,height2)+10);
-//    }
-//    else
-//    {
-//        $('html').height(Math.max(height1,height3,height2));
-//        $('body').height(Math.max(height1,height3,height2));
-//    }
-//
-//}
 $(document).ready(function () {
 
-    var p1 = $("#wtf");
+    var p1 = $("#sideMenu");
     var offset1 = p1.offset();
     console.log(offset1);
 
-    $('#wtf').animate({right: "+=150"}, 1);
+    $('#sideMenu').animate({right: "+=150"}, 1);
 
-    var p2 = $("#wtf");
+    var p2 = $("#sideMenu");
     var offset2 = p2.offset();
 
 
-    $('#wtf').mouseover(function() {
-        var p1 = $("#wtf");
+    $('#sideMenu').mouseover(function() {
+        var p1 = $("#sideMenu");
         var offset2 = p1.offset();
         console.log(offset1);
-        $("#wtf").offset({ top: offset2.top, left: offset1.left});
+        $("#sideMenu").offset({ top: offset2.top, left: offset1.left});
     })
 
-    $('#wtf').mouseleave(function() {
-        var p1 = $("#wtf");
+    $('#sideMenu').mouseleave(function() {
+        var p1 = $("#sideMenu");
         var offset3 = p1.offset();
-        $("#wtf").offset({ top: offset3.top, left: offset1.left});
-        $("#wtf").offset({ top: offset3.top, left: -151.916671752929688});
+        $("#sideMenu").offset({ top: offset3.top, left: offset1.left});
+        $("#sideMenu").offset({ top: offset3.top, left: -151.916671752929688});
     });
 
     $('.nav li').click(function () {
